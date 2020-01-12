468c8d0d7a37ab9f0df819ebccacab5a2a8a570f
Mark Karpeles <magicaltux@gmail.com>
2020-01-13 08:16:06 +0900

    memkv: use llrb instead of map to keep order

--- memkv/kv.go
+++ memkv/kv.go
@@ -6,30 +6,37 @@ import (
 	"sync"
 
 	"github.com/KarpelesLab/vfs"
+	"github.com/petar/GoLLRB/llrb"
 )
 
-type entry []byte
+type entry struct {
+	key  string
+	data []byte
+}
+
+type entryStart struct{}
+
 type memKv struct {
-	data map[string]entry
+	data *llrb.LLRB
 	lk   sync.RWMutex
 }
 
 func New() vfs.Keyval {
 	return &memKv{
-		data: make(map[string]entry),
+		data: llrb.New(),
 	}
 }
 
 func (e *memKv) Get(key string) (vfs.KVEntry, error) {
 	e.lk.RLock()
-	f, ok := e.data[key]
+	f := e.data.Get(&entry{key: key})
 	e.lk.RUnlock()
 
-	if !ok {
+	if f == nil {
 		return nil, os.ErrNotExist
 	}
 
-	return f, nil
+	return f.(*entry), nil
 }
 
 func (e *memKv) Put(key string, value vfs.KVEntry) error {
@@ -39,7 +46,7 @@ func (e *memKv) Put(key string, value vfs.KVEntry) error {
 	}
 
 	e.lk.Lock()
-	e.data[key] = entry(data)
+	e.data.ReplaceOrInsert(&entry{key: key, data: data})
 	e.lk.Unlock()
 
 	return nil
@@ -47,31 +54,35 @@ func (e *memKv) Put(key string, value vfs.KVEntry) error {
 
 func (e *memKv) Delete(key string) error {
 	e.lk.Lock()
-	delete(e.data, key)
+	e.data.Delete(&entry{key: key})
 	e.lk.Unlock()
 	return nil
 }
 
 func (e memKv) List(prefix string, callback func(key string, value vfs.KVEntry) (bool, error)) error {
-	cop := make(map[string]entry)
+	var list []*entry
 
 	// make a copy so callback can use functions that use a lock
 	e.lk.RLock()
 	if prefix == "" {
-		for k, v := range e.data {
-			cop[k] = v
-		}
+		e.data.AscendGreaterOrEqual(entryStart{}, func(i llrb.Item) bool {
+			list = append(list, i.(*entry))
+			return true
+		})
 	} else {
-		for k, v := range e.data {
-			if strings.HasPrefix(k, prefix) {
-				cop[k] = v
+		e.data.AscendGreaterOrEqual(&entry{key: prefix}, func(i llrb.Item) bool {
+			item := i.(*entry)
+			if !strings.HasPrefix(item.key, prefix) {
+				return false
 			}
-		}
+			list = append(list, item)
+			return true
+		})
 	}
 	e.lk.RUnlock()
 
-	for k, v := range cop {
-		cont, err := callback(k, v)
+	for _, e := range list {
+		cont, err := callback(e.key, e)
 		if err != nil {
 			return err
 		}
@@ -82,6 +93,21 @@ func (e memKv) List(prefix string, callback func(key string, value vfs.KVEntry) 
 	return nil
 }
 
-func (d entry) Data() ([]byte, error) {
-	return []byte(d), nil
+func (d *entry) Data() ([]byte, error) {
+	return d.data, nil
+}
+
+func (d *entry) Less(than llrb.Item) bool {
+	switch e := than.(type) {
+	case *entry:
+		return strings.Compare(d.key, e.key) < 0
+	case entryStart:
+		return false // always higher than start
+	}
+
+	return false // ???
+}
+
+func (e entryStart) Less(than llrb.Item) bool {
+	return true // start is always less
 }
