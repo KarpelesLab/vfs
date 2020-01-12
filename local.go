0ddf202512d9d115e00e9e798b9bb4e6c904ce31
Mark Karpeles <magicaltux@gmail.com>
2020-01-12 17:39:33 +0900

    do not expose localFS itself

--- local.go
+++ local.go
@@ -6,7 +6,7 @@ import (
 	"path/filepath"
 )
 
-type LocalFS struct {
+type localFS struct {
 	root string
 }
 
@@ -15,10 +15,10 @@ type LocalFile os.File
 // NewLocal creates a new local filesystem with root as root point. Note that
 // the root argument format depends on filesystem.
 func NewLocal(root string) (FileSystem, error) {
-	return &LocalFS{root}, nil
+	return &localFS{root}, nil
 }
 
-func (l *LocalFS) doPath(p string) string {
+func (l *localFS) doPath(p string) string {
 	p = path.Clean(p)
 	if l.root == "" {
 		return filepath.FromSlash(p)
@@ -26,7 +26,7 @@ func (l *LocalFS) doPath(p string) string {
 	return filepath.Join(l.root, filepath.FromSlash(p))
 }
 
-func (l *LocalFS) Open(name string) (File, error) {
+func (l *localFS) Open(name string) (File, error) {
 	f, err := os.Open(l.doPath(name))
 	if err != nil {
 		return nil, err
@@ -35,7 +35,7 @@ func (l *LocalFS) Open(name string) (File, error) {
 	return (*LocalFile)(f), nil
 }
 
-func (l *LocalFS) OpenFile(name string, flag int, perm os.FileMode) (File, error) {
+func (l *localFS) OpenFile(name string, flag int, perm os.FileMode) (File, error) {
 	f, err := os.OpenFile(l.doPath(name), flag, perm)
 	if err != nil {
 		return nil, err
@@ -44,7 +44,7 @@ func (l *LocalFS) OpenFile(name string, flag int, perm os.FileMode) (File, error
 	return (*LocalFile)(f), nil
 }
 
-func (l *LocalFS) Chroot(name string) (FileSystem, error) {
+func (l *localFS) Chroot(name string) (FileSystem, error) {
 	p := l.doPath(name)
 
 	p, err := filepath.Abs(p)
@@ -66,7 +66,7 @@ func (l *LocalFS) Chroot(name string) (FileSystem, error) {
 		return nil, ErrNotDirectory
 	}
 
-	return &LocalFS{p}, nil
+	return &localFS{p}, nil
 }
 
 func (f *LocalFile) Close() error {
