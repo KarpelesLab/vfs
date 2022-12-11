d8b4bdec92539aa1c1cf417cf9c91dab45bc3fd6
Mark Karpeles <magicaltux@gmail.com>
2022-12-11 14:38:27 +0900

    replace os→fs

--- intf.go
+++ intf.go
@@ -2,23 +2,23 @@ package vfs
 
 import (
 	"io"
-	"os"
+	"io/fs"
 )
 
 type FileSystem interface {
 	Open(name string) (File, error)
-	OpenFile(path string, flag int, perm os.FileMode) (File, error)
-	// Lstat returns the os.FileInfo for the given path, without
+	OpenFile(path string, flag int, perm fs.FileMode) (File, error)
+	// Lstat returns the fs.FileInfo for the given path, without
 	// following symlinks.
-	Lstat(path string) (os.FileInfo, error)
-	// Stat returns the os.FileInfo for the given path, following
+	Lstat(path string) (fs.FileInfo, error)
+	// Stat returns the fs.FileInfo for the given path, following
 	// symlinks.
-	Stat(path string) (os.FileInfo, error)
+	Stat(path string) (fs.FileInfo, error)
 	// Mkdir creates a directory at the given path. If the directory
 	// already exists or its parent directory does not exist or
 	// the permissions don't allow it, an error will be returned. See
 	// also the shorthand function MkdirAll.
-	Mkdir(path string, perm os.FileMode) error
+	Mkdir(path string, perm fs.FileMode) error
 	// Remove removes the item at the given path. If the path does
 	// not exists or the permissions don't allow removing it or it's
 	// a non-empty directory, an error will be returned. See also
@@ -33,8 +33,8 @@ type File interface {
 	io.Writer
 	io.WriterAt
 	io.Seeker
-	Readdir(count int) ([]os.FileInfo, error)
-	Stat() (os.FileInfo, error)
+	Readdir(count int) ([]fs.FileInfo, error)
+	Stat() (fs.FileInfo, error)
 }
 
 type Keyval interface {
